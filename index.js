b9bbda1910fdfafdf8520cd9b2d3cb0d3739701a
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-18 01:40:53 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json")
+const fs = require("fs");
+
+client.on("ready", async() => {
+  client.user.setActivity("In Developement!")
+  console.log(`I am logged in as ${client.user.tag}`)
+})
+
+bot.login(process.evl.TOKEN)
